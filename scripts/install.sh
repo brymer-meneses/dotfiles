da1260b409c8337a9123cc357f8e41b2105caea4
Brymer Meneses <68867487+brymer-meneses@users.noreply.github.com>
2021-04-29 22:03:29 +0800

    trivial changes

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,16 @@
 
 # 
 echo 'Installing Stow'
-stow -t ~ */
+git submodule update --init --recursive
+
+
+stow --target "$HOME" 		       alacritty
+stow --target "$HOME" --no-folding VSCodium
+stow --target "$HOME" 		       nvim
+stow --target "$HOME" 	           alacritty
+stow --target "$HOME" 	           zathura
+stow --target "$HOME" 	           spicetify
+stow --target "$HOME" 		       gnome-themes --dotfiles
+
+
 echo 'Successfully installed dotfiles'
