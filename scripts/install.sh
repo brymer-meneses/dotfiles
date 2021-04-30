06bb27eb6cd0a277c8f6ed9e2811dc5333695aa7
Brymer Meneses <68867487+brymer-meneses@users.noreply.github.com>
2021-04-30 16:10:49 +0800

    prevent stowing files to the wrong directories

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,16 +2,21 @@
 
 echo 'Installing dotfiles'
 
-# Updates the submodules
-git submodule update --recursive --remote
+# Check if the script is being NOT being
+# used in the dotfiles directory
+if [ ! -f ".stow" ]; then 
+    echo "This script should be executed at the root of the dotfiles directory!"
+    exit 1
+fi
 
-stow --target "$HOME" 		       alacritty
+# Updates the submodules
+git pull --recurse-submodules
+
+stow --target "$HOME" 		   alacritty
 stow --target "$HOME" --no-folding VSCodium
-stow --target "$HOME" 		       nvim
-stow --target "$HOME" 	           alacritty
+stow --target "$HOME" 		   nvim
 stow --target "$HOME" 	           zathura
 stow --target "$HOME" 	           spicetify
-stow --target "$HOME" 		       gnome-themes --dotfiles
-
+stow --target "$HOME" 		   gnome-themes --dotfiles
 
 echo 'Successfully installed dotfiles'
